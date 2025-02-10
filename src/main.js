ed5db3af3eae786c84ad3e199889b4cdd768af56
Jeshan1 <jectionjection@gmail.com>
2025-02-10 15:05:41 +0545

    just for fun

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import route from './router/route'
+import index from './store/index'
+import './assets/tailwind.css'
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css'; // Import the styles for Toast
 
 createApp(App)
+.use(index)
+.use(Toast)
 .use(route)
 .mount('#app')
