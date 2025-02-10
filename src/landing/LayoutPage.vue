c0f3746f06c610172481c0368ce0240c8fdce5c3
Jeshan1 <jectionjection@gmail.com>
2025-02-10 15:05:45 +0545

    just for fun

---
+++ src/landing/LayoutPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <NavComponent/>
+    <div>
+        <h1>This is content</h1>
+    </div>
+    <FooterComponent/>
+</template>
+
+<script setup>
+import NavComponent from '../components/NavComponent.vue'
+import FooterComponent from '../components/FooterComponent.vue'
+
+</script>
