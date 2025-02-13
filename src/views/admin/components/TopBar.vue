a4e64d9ee92c6c7d72fe00fd91dd6bfced9d84dc
Jeshan1 <jectionjection@gmail.com>
2025-02-13 16:41:32 +0545

    just for fun

--- src/views/admin/components/TopBar.vue
+++ src/views/admin/components/TopBar.vue
@@ -1,20 +1,31 @@
 <template>
-    <div class="bg-gray-800 text-white flex items-center justify-end p-4 fixed top-0 right-0 left-[250px]">
-      <!-- Right side: User Info & Dropdown -->
-      <div class="relative">
-        <button @click="toggleDropdown" class="bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 rounded-full" >
-          User
-        </button>
-        
-        <!-- Dropdown Menu -->
-        <div v-if="showDropdown" class="absolute right-0 mt-2 w-48 bg-white text-black rounded-lg shadow-lg z-10">
-          <ul class="cursor-pointer bg-white p-4">
-            <a class="px-4 py-2 hover:bg-gray-100 cursor-pointer" @click="logout">Logout</a>
-          </ul>
-        </div>
+  <div class="bg-gray-800 text-white flex items-center justify-end p-4 fixed top-0 right-0 left-[250px]">
+    <!-- Right side: User Info & Dropdown -->
+    <div class="relative">
+      <button 
+        @click="toggleDropdown" 
+        class="bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 rounded-full"
+      >
+        User
+      </button>
+
+      <!-- Dropdown Menu -->
+      <div 
+        v-if="showDropdown" 
+        class="fixed right-28 top-2 w-48 bg-black text-white rounded shadow-lg z-[1000]"
+      >
+        <ul class="cursor-pointer bg-black p-4 rounded">
+          <li>
+            <a class="block px-4 py-2 hover:bg-gray-700 cursor-pointer text-white" @click="logout">
+              Logout
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
-  </template>
+  </div>
+</template>
+
   
   <script setup>
   import { ref } from 'vue'
