36c3bfed778ab779a26954ae98f0e723f0cc9aa6
Jeshan1 <jectionjection@gmail.com>
2025-02-13 10:30:27 +0545

    just for fun

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 import auth from "./AuthStore";
 import category from "./CategoryStore";
+import blog from "./BlogStore"
 
 export default createStore({
   modules: {
     auth,
-    category
+    category,
+    blog
   },
 });
