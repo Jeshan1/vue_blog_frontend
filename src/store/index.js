7ca56f01919555a3e20a64a71091de27f4086e10
Jeshan1 <jectionjection@gmail.com>
2025-02-13 15:03:20 +0545

    just for fun

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import auth from "./AuthStore";
 import category from "./CategoryStore";
 import blog from "./BlogStore"
 import contact from "./ContactStore"
+import comment from "./CommentStore"
 
 export default createStore({
   modules: {
     auth,
     category,
     blog,
-    contact
+    contact,
+    comment
   },
 });
