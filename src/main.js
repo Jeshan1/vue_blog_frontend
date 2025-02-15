ad7de020290cca2752a8173a7b71931fc83c567d
Jeshan1 <jectionjection@gmail.com>
2025-02-16 00:23:58 +0545

    just for fun

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import index from './store/index'
 import './assets/tailwind.css'
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css'; // Import the styles for Toast
-import Chart from 'primevue/chart'
+import Prime from 'primevue/config'
 createApp(App)
 .use(index)
 .use(Toast)
 .use(route)
-.use(Chart)
+.use(Prime, { ripple: true })
 .mount('#app')
