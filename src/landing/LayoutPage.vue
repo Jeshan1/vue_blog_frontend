26f45dd343983c61c727c464c1f7dc16f138e4fb
Jeshan1 <jectionjection@gmail.com>
2025-02-24 11:18:08 +0545

    just for learn

--- src/landing/LayoutPage.vue
+++ src/landing/LayoutPage.vue
@@ -4,11 +4,12 @@
         <router-view></router-view>
     </div>
     <FooterComponent />
+    <GoToTopButton/>
 </template>
 
 <script setup>
 
 import NavComponent from '../components/NavComponent.vue'
 import FooterComponent from '../components/FooterComponent.vue'
-
+import GoToTopButton from '@/components/GoToTopButton.vue'
 </script>
