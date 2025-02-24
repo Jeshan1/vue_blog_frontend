e546c336e0467ea4c764460a9caacea5c0be8811
Jeshan1 <jectionjection@gmail.com>
2025-02-24 10:43:14 +0545

    just for learn

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -4,14 +4,14 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           <div class="lg:flex lg:items-center">
             <a href="javascript:void(0)">
-              <img src="https://readymadeui.com/readymadeui-light.svg" alt="logo" class="w-48" />
+              <img src="logo.png" alt="logo" class="w-48" />
             </a>
           </div>
 
           <div class="lg:flex lg:items-center">
             <ul class="flex space-x-6">
               <li>
-                <a href="javascript:void(0)">
+                <a href="https://www.facebook.com/jection.jection.564/">
                   <svg xmlns="http://www.w3.org/2000/svg" class="fill-gray-300 hover:fill-white w-7 h-7" viewBox="0 0 24 24">
                     <path fill-rule="evenodd"
                       d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h7v-7h-2v-3h2V8.5A3.5 3.5 0 0 1 15.5 5H18v3h-2a1 1 0 0 0-1 1v2h3v3h-3v7h4a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2z"
@@ -20,7 +20,7 @@
                 </a>
               </li>
               <li>
-                <a href="javascript:void(0)">
+                <a href="https://www.linkedin.com/in/jeshan-tiwari-012ab527b/">
                   <svg xmlns="http://www.w3.org/2000/svg" class="fill-gray-300 hover:fill-white w-7 h-7" viewBox="0 0 24 24">
                     <path fill-rule="evenodd"
                       d="M21 5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5zm-2.5 8.2v5.3h-2.79v-4.93a1.4 1.4 0 0 0-1.4-1.4c-.77 0-1.39.63-1.39 1.4v4.93h-2.79v-8.37h2.79v1.11c.48-.78 1.47-1.3 2.32-1.3 1.8 0 3.26 1.46 3.26 3.26zM6.88 8.56a1.686 1.686 0 0 0 0-3.37 1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68zm1.39 1.57v8.37H5.5v-8.37h2.77z"
@@ -44,13 +44,13 @@
             <h4 class="text-lg mb-6 text-white">Useful links</h4>
             <ul class="space-y-4 pl-2">
               <li>
-                <a href="javascript:void(0)" class="text-gray-300 hover:text-white text-sm">Featured</a>
+                <router-link to="/features" class="text-gray-300 hover:text-white text-sm">Featured</router-link>
               </li>
               <li>
-                <a href="javascript:void(0)" class="text-gray-300 hover:text-white text-sm">New Arrivals</a>
+                <router-link to="/blogs" class="text-gray-300 hover:text-white text-sm">Blogs</router-link>
               </li>
               <li>
-                <a href="javascript:void(0)" class="text-gray-300 hover:text-white text-sm">New Arrivals</a>
+                <router-link to="/home" class="text-gray-300 hover:text-white text-sm">New Arrivals</router-link>
               </li>
             </ul>
           </div>
@@ -59,7 +59,7 @@
             <h4 class="text-lg mb-6 text-white">Information</h4>
             <ul class="space-y-4 pl-2">
               <li>
-                <a href="javascript:void(0)" class="text-gray-300 hover:text-white text-sm">About Us</a>
+                <router-link to="/about" class="text-gray-300 hover:text-white text-sm">About Us</router-link>
               </li>
               <li>
                 <a href="javascript:void(0)" class="text-gray-300 hover:text-white text-sm">Terms &amp; Conditions</a>
@@ -68,10 +68,10 @@
                 <a href="javascript:void(0)" class="text-gray-300 hover:text-white text-sm">Privacy Policy</a>
               </li>
               <li>
-                <a href="javascript:void(0)" class="text-gray-300 hover:text-white text-sm">Sale</a>
+                <router-link to="/contact" class="text-gray-300 hover:text-white text-sm">Contact Us</router-link>
               </li>
               <li>
-                <a href="javascript:void(0)" class="text-gray-300 hover:text-white text-sm">Documentation</a>
+                <a href="https://github.com/Jeshan1/vue_blog_frontend" class="text-gray-300 hover:text-white text-sm">Documentation</a>
               </li>
             </ul>
           </div>
